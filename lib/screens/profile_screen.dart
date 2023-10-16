4b26085d3c1d449baeefa352b64283773121b2d0
Josip Domazet <josip.domazet.corp@gmail.com>
2023-10-16 23:07:00 +0200

    added 4 bottom tabs

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home Content'),
+      ),
+    );
+  }
+}

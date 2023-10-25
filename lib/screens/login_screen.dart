7bea5ef74464053cd3e9932a6f360d947cdae4bd
Josip Domazet <josip.domazet.corp@gmail.com>
2023-10-25 02:04:44 +0200

    feat: restyled login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,34 +9,79 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+   //#7BED9F
+    // 👉🏽
+    // #D3D3D3
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Username',
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            radius: 2.0,
+            center: Alignment.topLeft,
+            colors: [
+              Color(0xFF7BED9F),
+              Color(0xFFD3D3D3)
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // App icon and name
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset('assets/icons/android/foreground.png',
+                        height: 128.0, width: 128.0),
+                    Text('PokéMap',
+                        style: TextStyle(
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ))
+                  ],
+                ),
               ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'Password',
+              SizedBox(height: 32.0),
+
+              // Login form
+              Card(
+                elevation: 8.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Username',
+                        ),
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        obscureText: true,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                bloc.add(AttemptLoginEvent());
-              },
-              child: Text('Login'),
-            ),
-          ],
+              SizedBox(height: 16.0),
+
+              // Login button
+              ElevatedButton(
+                onPressed: () {
+                  bloc.add(AttemptLoginEvent());
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
         ),
       ),
     );
